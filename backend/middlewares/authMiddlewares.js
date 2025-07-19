efa77d9938448a581dcec32eee6232f3a63ab4fe
paul kiragu <paulkiragumburu@gmail.com>
2025-07-19 20:35:18 +0300

    added protect and authorize middlewares

---
+++ backend/middlewares/authMiddlewares.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.protect = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token failed" });
+  }
+};
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Forbidden: Access denied" });
+    }
+    next();
+  };
+};
+
+
+exports.isInstructorOrAdmin = (req, res, next) => {
+  if (req.user && (req.user.role === 'instructor' || req.user.role === 'admin')) {
+    next();
+  } else {
+    res.status(403).json({ message: 'Not authorized as instructor or admin' });
+  }
+};
