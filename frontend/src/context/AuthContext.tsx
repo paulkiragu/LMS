7585104332805c3404b6cd83989931d752c7c64e
paulkiragu <123287301+paulkiragu@users.noreply.github.com>
2025-07-21 18:49:31 +0300

    Update AuthContext.tsx

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
   
   useEffect(() => {
