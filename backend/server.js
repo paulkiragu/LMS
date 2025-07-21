ebc519d5d3231a88531eb067e59db44a4edc0076
paul kiragu <paulkiragumburu@gmail.com>
2025-07-21 15:41:45 +0300

    changed the moongose connect to production

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/api/admin/stats', adminStatsRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_PRODUCTION)
   .then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
