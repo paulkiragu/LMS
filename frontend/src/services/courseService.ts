01a4e8cef8321a88091a45685a0057cc0454b3d4
paulkiragu <123287301+paulkiragu@users.noreply.github.com>
2025-07-23 10:15:31 +0300

    Update courseService.ts

--- frontend/src/services/courseService.ts
+++ frontend/src/services/courseService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || https://lmsbackend-63yn.onrender.com/api;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
